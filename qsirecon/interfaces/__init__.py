2580bce2e3d3b745cb2f055ac30ed7cf745798d9
Taylor Salo <tsalo006@fiu.edu>
2024-08-09 14:00:34 -0400

    Update __init__.py

--- qsirecon/interfaces/__init__.py
+++ qsirecon/interfaces/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-from .bids import (
-    DerivativesDataSink,
-)
+from .bids import DerivativesDataSink
 from .images import ConformDwi, ValidateImage
 from .reports import AboutSummary, SubjectSummary
 from .surf import NormalizeSurf
