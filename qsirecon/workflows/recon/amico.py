9e3edb6202525eeb24099296903b56c3aad5d50c
Taylor Salo <tsalo006@fiu.edu>
2024-08-14 16:44:16 -0400

    Refactor AMICO workflow a bit.

--- qsirecon/workflows/recon/amico.py
+++ qsirecon/workflows/recon/amico.py
@@ -47,9 +47,9 @@ def init_amico_noddi_fit_wf(
         fibgz
 
     """
-
     inputnode = pe.Node(
-        niu.IdentityInterface(fields=recon_workflow_input_fields + ["odf_rois"]), name="inputnode"
+        niu.IdentityInterface(fields=recon_workflow_input_fields + ["odf_rois"]),
+        name="inputnode",
     )
     outputnode = pe.Node(
         niu.IdentityInterface(
@@ -61,22 +61,17 @@ def init_amico_noddi_fit_wf(
                 "config_file",
                 "fibgz",
                 "recon_scalars",
-            ]
+            ],
         ),
         name="outputnode",
     )
     omp_nthreads = config.nipype.omp_nthreads
     workflow = Workflow(name=name)
-    recon_scalars = pe.Node(
-        AMICOReconScalars(qsirecon_suffix=qsirecon_suffix),
-        name="recon_scalars",
-        run_without_submitting=True,
-    )
+
     plot_reports = params.pop("plot_reports", True)
     desc = """NODDI Reconstruction
 
 : """
-    noddi_fit = pe.Node(NODDI(**params), name="recon_noddi", n_procs=omp_nthreads)
     desc += """\
 The NODDI model (@noddi) was fit using the AMICO implementation (@amico).
 A value of %.1E was used for parallel diffusivity and %.1E for isotropic
@@ -87,33 +82,45 @@ diffusivity.""" % (
     if params.get("is_exvivo"):
         desc += " An additional component was added to the model foe ex-vivo data."
 
+    recon_scalars = pe.Node(
+        AMICOReconScalars(qsirecon_suffix=qsirecon_suffix),
+        name="recon_scalars",
+        run_without_submitting=True,
+    )
+    noddi_fit = pe.Node(NODDI(**params), name="recon_noddi", n_procs=omp_nthreads)
     convert_to_fibgz = pe.Node(NODDItoFIBGZ(), name="convert_to_fibgz")
 
     workflow.connect([
-        (inputnode, noddi_fit, [('dwi_file', 'dwi_file'),
-                                ('bval_file', 'bval_file'),
-                                ('bvec_file', 'bvec_file'),
-                                ('dwi_mask', 'mask_file')]),
+        (inputnode, noddi_fit, [
+            ('dwi_file', 'dwi_file'),
+            ('bval_file', 'bval_file'),
+            ('bvec_file', 'bvec_file'),
+            ('dwi_mask', 'mask_file'),
+        ]),
         (noddi_fit, outputnode, [
             ('directions_image', 'directions_image'),
             ('icvf_image', 'icvf_image'),
             ('od_image', 'od_image'),
             ('isovf_image', 'isovf_image'),
-            ('config_file', 'config_file')]),
+            ('config_file', 'config_file'),
+        ]),
         (noddi_fit, recon_scalars, [
             ('icvf_image', 'icvf_image'),
             ('od_image', 'od_image'),
             ('isovf_image', 'isovf_image'),
-            ('directions_image', 'directions_image')]),
+            ('directions_image', 'directions_image'),
+        ]),
         (recon_scalars, outputnode, [("scalar_info", "recon_scalars")]),
         (noddi_fit, convert_to_fibgz, [
             ('directions_image', 'directions_file'),
             ('icvf_image', 'icvf_file'),
             ('od_image', 'od_file'),
-            ('isovf_image', 'isovf_file')]),
+            ('isovf_image', 'isovf_file'),
+        ]),
         (inputnode, convert_to_fibgz, [('dwi_mask', 'mask_file')]),
         (convert_to_fibgz, outputnode, [('fibgz_file', 'fibgz')])
     ])  # fmt:skip
+
     if plot_reports:
         plot_peaks = pe.Node(CLIReconPeaksReport(), name="plot_peaks", n_procs=omp_nthreads)
         ds_report_peaks = pe.Node(
@@ -126,7 +133,6 @@ diffusivity.""" % (
             name="ds_report_peaks",
             run_without_submitting=True,
         )
-
         workflow.connect([
             (inputnode, plot_peaks, [('dwi_mask', 'mask_file')]),
             (convert_to_fibgz, plot_peaks, [('fibgz_file', 'fib_file')]),
@@ -137,25 +143,25 @@ diffusivity.""" % (
     if qsirecon_suffix:
         ds_fibgz = pe.Node(
             DerivativesDataSink(
+                dismiss_entities=("desc",),
                 extension="fib.gz",
                 compress=True,
             ),
-            name="ds_{}_fibgz".format(qsirecon_suffix),
+            name=f"ds_{qsirecon_suffix}_fibgz",
             run_without_submitting=True,
         )
-        workflow.connect(outputnode, 'fibgz', ds_fibgz, 'in_file')  # fmt:skip
+        workflow.connect([(outputnode, ds_fibgz, [("fibgz", "in_file")])])
 
         ds_recon_scalars = pe.Node(
-            ReconScalarsDataSink(), name="ds_recon_scalars", run_without_submitting=True
+            ReconScalarsDataSink(),
+            name="ds_recon_scalars",
+            run_without_submitting=True,
         )
-        workflow.connect(
-            recon_scalars,
-            "scalar_info",
-            ds_recon_scalars,
-            "recon_scalars")  # fmt:skip
+        workflow.connect([(recon_scalars, ds_recon_scalars, [("scalar_info", "recon_scalars")])])
 
         ds_config = pe.Node(
             DerivativesDataSink(
+                dismiss_entities=("desc",),
                 mfp="AMICOconfig",
                 model="NODDI",
                 compress=True,
@@ -163,7 +169,7 @@ diffusivity.""" % (
             name="ds_noddi_config",
             run_without_submitting=True,
         )
-        workflow.connect(outputnode, "config_file", ds_config, "in_file")
+        workflow.connect([(outputnode, ds_config, [("config_file", "in_file")])])
 
     workflow.__desc__ = desc
 
