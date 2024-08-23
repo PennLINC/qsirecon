06e38f14a6b1d20c38730bcd954b8828ea6a087e
Matt Cieslak <mattcieslak@gmail.com>
2024-08-23 09:30:43 -0400

    use real file

--- qsirecon/workflows/recon/amico.py
+++ qsirecon/workflows/recon/amico.py
@@ -143,12 +143,10 @@ diffusivity.""" % (
         ])  # fmt:skip
 
     if qsirecon_suffix:
-        derivatives_config = load_yaml(load_data("scalars/amico_noddi.yaml"))
+        derivatives_config = load_yaml(load_data("nonscalars/amico_noddi.yaml"))
         ds_fibgz = pe.Node(
             DerivativesDataSink(
-                dismiss_entities=("desc",),
-                compress=True,
-                **derivatives_config["fibgz"]["bids"]
+                dismiss_entities=("desc",), compress=True, **derivatives_config["fibgz"]["bids"]
             ),
             name=f"ds_{qsirecon_suffix}_fibgz",
             run_without_submitting=True,
@@ -165,7 +163,7 @@ diffusivity.""" % (
             DerivativesDataSink(
                 dismiss_entities=("desc",),
                 compress=True,
-                **derivatives_config["config_file"]["bids"]
+                **derivatives_config["config_file"]["bids"],
             ),
             name="ds_noddi_config",
             run_without_submitting=True,
