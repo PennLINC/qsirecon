4a758b4e5407e2a831a2c1dd63e18145c6569cb6
Steven Meisler <smeisler@g.harvard.edu>
2024-11-18 15:40:16 -0500

    lint parser.py

--- qsirecon/cli/parser.py
+++ qsirecon/cli/parser.py
@@ -588,6 +588,7 @@ def parse_args(args=None, namespace=None):
         # Change the participants label based on ingression renaming
         if config.execution.participant_label is not None:
             config.execution.participant_label = [layout['subject'] for layout in layouts]
+
     else:
         config.execution.bids_dir = config.execution.input_dir
 
