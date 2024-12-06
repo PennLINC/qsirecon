f95211a763578efe5cd6f11cb906a8702bf8a9f5
Matt Cieslak <mattcieslak@gmail.com>
2024-12-06 14:33:17 -0500

    Add a node for selecting specific shells from preprocessed dwis (#194)
    ---------
    
    Co-authored-by: Taylor Salo <salot@pennmedicine.upenn.edu>

--- qsirecon/interfaces/gradients.py
+++ qsirecon/interfaces/gradients.py
@@ -2,12 +2,12 @@
 
 import logging
 
-import nibabel as nb
 import numpy as np
 from nilearn import image as nim
 from nipype.interfaces.base import (
     BaseInterfaceInputSpec,
     File,
+    InputMultiObject,
     SimpleInterface,
     TraitedSpec,
     isdefined,
@@ -18,33 +18,214 @@ from nipype.utils.filemanip import fname_presuffix
 LOGGER = logging.getLogger("nipype.interface")
 
 
-class RemoveDuplicatesInputSpec(BaseInterfaceInputSpec):
+class _GradientSelectInputSpec(BaseInterfaceInputSpec):
     dwi_file = File(exists=True, mandatory=True)
     bval_file = File(exists=True, mandatory=True)
     bvec_file = File(exists=True, mandatory=True)
+    b_file = File(exists=True)
+    btable_file = File(exists=True)
+    bval_distance_cutoff = traits.Float(5.0, usedefault=True)
+    expected_n_input_shells = traits.Int()
+    requested_shells = InputMultiObject(
+        traits.Either(traits.CInt(), traits.Enum("highest", "lowest")), mandatory=True
+    )
+
+
+class _GradientSelectOutputSpec(TraitedSpec):
+    dwi_file = File(exists=True)
+    bval_file = File(exists=True)
+    bvec_file = File(exists=True)
+    b_file = File(exists=True)
+    btable_file = File(exists=True)
+    local_bvec_file = File(exists=True)
+
+
+class GradientSelect(SimpleInterface):
+    input_spec = _GradientSelectInputSpec
+    output_spec = _GradientSelectOutputSpec
+
+    def _run_interface(self, runtime):
+        """Find shells in the input data and select"""
+        bvals = np.loadtxt(self.inputs.bval_file)
+
+        selected_indices = _select_gradients(
+            requested_gradients=self.inputs.requested_shells,
+            max_distance=self.inputs.bval_distance_cutoff,
+            original_bvals=bvals,
+            expected_n_input_shells=self.inputs.expected_n_input_shells or None,
+        )
+
+        # Make a new dwi and associated gradient files
+        new_bval, new_bvec, new_b, new_btable, new_nifti = subset_dwi(
+            original_bval=self.inputs.bval_file,
+            original_bvec=self.inputs.bvec_file,
+            original_b=self.inputs.b_file or None,
+            original_btable=self.inputs.btable_file or None,
+            original_nifti=self.inputs.dwi_file,
+            indices=selected_indices,
+            newdir=runtime.cwd,
+            suffix="_selected",
+        )
+        self._results["bval_file"] = new_bval
+        self._results["bvec_file"] = new_bvec
+        self._results["dwi_file"] = new_nifti
+        if new_b:
+            self._results["b_file"] = new_b
+        if new_btable:
+            self._results["btable_file"] = new_btable
+
+        return runtime
+
+
+def _select_gradients(
+    requested_gradients,
+    max_distance,
+    original_bvals,
+    expected_n_input_shells=None,
+):
+    """Find indices where bvals are within distance_max of desired_bval."""
+    from sklearn.metrics import pairwise_distances
+
+    # Get the bvals clustered into assigned shells. Also in a dataframe
+    bval_df = _find_shells(original_bvals, max_distance)
+
+    # Check that the correct number of input shells are detected
+    if expected_n_input_shells is not None:
+        n_found_shells = len(bval_df["assigned_shell"].unique())
+        if not expected_n_input_shells == n_found_shells:
+            howmuch, newthresh = (
+                ("too many", "higher")
+                if n_found_shells > expected_n_input_shells
+                else ("too few", "lower")
+            )
+            raise Exception(
+                f"Expected to find {expected_n_input_shells} shells in "
+                f"the input data. Instead we found {n_found_shells}. Having "
+                f"{howmuch} shells detected means you may need to adjust the"
+                f"bval_distance_cutoff parameter to be {newthresh}."
+            )
+
+    # Ensure that at lease 1 b>0 and one b=0 shell are selected
+    select_shell_bs = _parse_shell_selection(requested_gradients, bval_df, max_distance)
+
+    # Make sure the shells are unique (no overlap when accounting for allowed distances)
+    shell_distances = pairwise_distances(select_shell_bs.reshape(-1, 1))
+    if np.any(shell_distances[np.triu_indices_from(shell_distances, k=1)] < max_distance):
+        raise Exception(
+            "Shells and bval_distance_cutoff have overlap. Choose a lower "
+            "bval_distance_cutoff or more separated shells."
+        )
+
+    selected_indices = np.flatnonzero(bval_df["assigned_shell"].isin(select_shell_bs))
+
+    return selected_indices
+
+
+def _parse_shell_selection(requested_bvals, bval_df, max_distance):
+    """Turn bval requests into numbers. Add a 0 if none were originally included."""
+    numeric_bvals = []
+    for requested_bval in requested_bvals:
+        if requested_bval == "highest":
+            highest_shell = bval_df["assigned_shell"].max()
+            LOGGER.info(f"Selecting b={highest_shell} as the highest shell")
+            numeric_bvals.append(highest_shell)
+        elif requested_bval == "lowest":
+            lowest_shell = bval_df["assigned_shell"][
+                bval_df["assigned_shell"] > max_distance
+            ].min()
+            LOGGER.info(f"Selecting b={lowest_shell} as the lowest b>0 shell")
+            numeric_bvals.append(lowest_shell)
+        else:
+            # Find the closest detected shell
+            ok_shells = bval_df["assigned_shell"][
+                np.logical_and(
+                    bval_df["assigned_shell"] >= requested_bval - max_distance,
+                    bval_df["assigned_shell"] <= requested_bval + max_distance,
+                )
+            ]
+            if len(ok_shells.unique()) > 1:
+                raise Exception(
+                    f"Unable to unambiguously select b={requested_bval}. "
+                    f"Instead select from {bval_df.assigned_shells.unique().tolist()}"
+                )
+
+            numeric_bvals.append(ok_shells.iloc[0])
+
+    # Make sure there is a 0 in the list
+    if len(numeric_bvals) < 2:
+        if numeric_bvals[0] < max_distance:
+            LOGGER.critical("No effectively b>0 shells are selected.")
+        else:
+            LOGGER.warning("Adding a b=0 shell to the selection.")
+            numeric_bvals.append(0)
+
+    return np.array(sorted(numeric_bvals))
+
+
+def _find_shells(bvals, max_distance):
+
+    import pandas as pd
+    from sklearn.cluster import AgglomerativeClustering
+    from sklearn.metrics import silhouette_score
+
+    X = bvals.reshape(-1, 1)
+    agg_cluster = AgglomerativeClustering(
+        n_clusters=None,
+        distance_threshold=2 * max_distance,
+        linkage="complete",
+    ).fit(X)
+    shells = agg_cluster.labels_
+
+    # Introduce a new check:
+    score = silhouette_score(X, shells)
+    if score < 0.8:
+        raise Exception("Silhouette score is low. Is this is a DSI scheme?")
+
+    # Do the same check as mrtrix
+    max_shells = np.sqrt(np.sum(bvals > max_distance))
+    if agg_cluster.n_clusters_ > max_shells:
+        raise Exception("Too many possible shells detected.")
+
+    bval_df = pd.DataFrame({"bvalue": bvals, "assignment": shells})
+    shell_df = bval_df.groupby("assignment", as_index=False).agg({"bvalue": "median"})
+    bval_df["assigned_shell"] = bval_df["assignment"].replace(
+        shell_df["assignment"].tolist(), shell_df["bvalue"].tolist()
+    )
+    bval_df["shell_num"] = bval_df["assigned_shell"].rank(method="dense")
+    bval_df.drop(columns=["assignment"], inplace=True)
+
+    return bval_df
+
+
+class _RemoveDuplicatesInputSpec(BaseInterfaceInputSpec):
+    dwi_file = File(exists=True, mandatory=True)
+    bval_file = File(exists=True, mandatory=True)
+    bvec_file = File(exists=True, mandatory=True)
+    b_file = File(exists=True)
+    btable_file = File(exists=True)
     local_bvec_file = File(exists=True)
     distance_cutoff = traits.Float(5.0, usedefault=True)
     expected_directions = traits.Int()
 
 
-class RemoveDuplicatesOutputSpec(TraitedSpec):
+class _RemoveDuplicatesOutputSpec(TraitedSpec):
     dwi_file = File(exists=True)
     bval_file = File(exists=True)
     bvec_file = File(exists=True)
+    b_file = File(exists=True)
+    btable_file = File(exists=True)
     local_bvec_file = File(exists=True)
 
 
 class RemoveDuplicates(SimpleInterface):
-    input_spec = RemoveDuplicatesInputSpec
-    output_spec = RemoveDuplicatesOutputSpec
+    input_spec = _RemoveDuplicatesInputSpec
+    output_spec = _RemoveDuplicatesOutputSpec
 
     def _run_interface(self, runtime):
         bvecs = np.loadtxt(self.inputs.bvec_file).T
         bvals = np.loadtxt(self.inputs.bval_file).squeeze()
-        orig_bvals = bvals.copy()
         bvals = np.sqrt(bvals - bvals.min())
         bvals = bvals / bvals.max() * 100
-        original_image = nb.load(self.inputs.dwi_file)
         cutoff = self.inputs.distance_cutoff
 
         scaled_bvecs = bvals[:, np.newaxis] * bvecs
@@ -77,32 +258,25 @@ class RemoveDuplicates(SimpleInterface):
                     "Expected %d unique samples but found %d", expected, len(seen_vecs)
                 )
 
-        # Are all the directions unique?
-        if len(ok_vecs) == len(bvals):
-            self._results["dwi_file"] = self.inputs.dwi_file
-            self._results["bval_file"] = self.inputs.bval_file
-            self._results["bvec_file"] = self.inputs.bvec_file
-            self._results["local_bvec_file"] = self.inputs.local_bvec_file
-
-            return runtime
-
         # Extract the unique samples
-        output_bval = fname_presuffix(self.inputs.bval_file, newpath=runtime.cwd, suffix="_unique")
-        output_bvec = fname_presuffix(self.inputs.bvec_file, newpath=runtime.cwd, suffix="_unique")
-        output_nii = fname_presuffix(self.inputs.dwi_file, newpath=runtime.cwd, suffix="_unique")
-        unique_indices = np.array(ok_vecs)
-        unique_bvals = orig_bvals[unique_indices]
-        np.savetxt(output_bval, unique_bvals, fmt="%d", newline=" ")
-        unique_bvecs = bvecs[unique_indices]
-        np.savetxt(output_bvec, unique_bvecs.T, fmt="%.8f")
-        unique_data = original_image.get_fdata()[..., unique_indices]
-        nb.Nifti1Image(unique_data, original_image.affine, original_image.header).to_filename(
-            output_nii
+        new_bval, new_bvec, new_b, new_btable, new_nifti = subset_dwi(
+            original_bval=self.inputs.bval_file,
+            original_bvec=self.inputs.bvec_file,
+            original_b=self.inputs.b_file or None,
+            original_btable=self.inputs.btable_file or None,
+            original_nifti=self.inputs.dwi_file,
+            indices=np.array(ok_vecs),
+            newdir=runtime.cwd,
+            suffix="_unique",
         )
-        self._results["bval_file"] = output_bval
-        self._results["bvec_file"] = output_bvec
-        self._results["dwi_file"] = output_nii
-        # TODO: support local bvecs
+        self._results["bval_file"] = new_bval
+        self._results["bvec_file"] = new_bvec
+        self._results["dwi_file"] = new_nifti
+        if new_b:
+            self._results["b_file"] = new_b
+        if new_btable:
+            self._results["btable_file"] = new_btable
+
         return runtime
 
 
@@ -176,3 +350,56 @@ def concatenate_bvecs(input_files):
     if not stacked.shape[1] == 3:
         stacked = stacked.T
     return stacked
+
+
+def subset_dwi(
+    original_bval,
+    original_bvec,
+    original_b,
+    original_btable,
+    original_nifti,
+    indices,
+    newdir,
+    suffix="_bsel",
+):
+    """Create a subset of a dwi based on a set of indices."""
+    bvals = np.loadtxt(original_bval)
+    if np.all(indices == np.arange(len(bvals))):
+        return original_bval, original_bvec, original_b, original_btable, original_nifti
+
+    # Subset and write the bval
+    new_bval = fname_presuffix(original_bval, newpath=newdir, suffix=suffix)
+    subsetted_bval_data = np.loadtxt(original_bval).squeeze()[indices]
+    np.savetxt(new_bval, subsetted_bval_data, fmt="%d", newline=" ")
+
+    # Subset and write the bvec
+    new_bvec = fname_presuffix(original_bvec, newpath=newdir, suffix=suffix)
+    selected_bvecs = np.loadtxt(original_bvec)[:, indices]
+    np.savetxt(new_bvec, selected_bvecs, fmt="%.8f")
+
+    # Subset and write the dwi nifti
+    new_nifti = fname_presuffix(original_nifti, newpath=newdir, suffix=suffix)
+    nim.index_img(original_nifti, indices).to_filename(new_nifti)
+
+    # If there is a b file, subset and write it
+    new_b = None
+    if original_b is not None:
+        new_b = fname_presuffix(original_b, newpath=newdir, suffix=suffix)
+        _select_lines(original_b, new_b, indices)
+
+    # If there is a dsi studio btable file, subset and write it
+    new_btable = None
+    if original_btable is not None:
+        new_btable = fname_presuffix(original_btable, newpath=newdir, suffix=suffix)
+        _select_lines(original_btable, new_btable, indices)
+    return new_bval, new_bvec, new_b, new_btable, new_nifti
+
+
+def _select_lines(in_file, out_file, indices):
+
+    with open(in_file, "r") as in_f:
+        in_lines = in_f.readlines()
+        new_lines = [in_lines[lineno] for lineno in indices]
+
+    with open(out_file, "w") as out_f:
+        out_f.writelines(new_lines)
---
+++ qsirecon/tests/test_interfaces.py
@@ -0,0 +1,80 @@
+"""Command-line interface tests."""
+
+from pathlib import Path
+
+import nibabel as nb
+import numpy as np
+import pytest
+
+from qsirecon.data import load as load_data
+from qsirecon.interfaces.gradients import GradientSelect, _find_shells
+from qsirecon.tests.utils import download_test_data, get_test_data_path
+
+
+@pytest.mark.interfaces
+def test_shell_selection(data_dir):
+    """Run reconstruction workflow tests."""
+    dwi_prefix = "sub-ABCD_acq-10per000_space-T1w_desc-preproc_dwi"
+    data_dir = "/home/matt/projects/qsirecon/.circleci/data"
+    dataset_dir = Path(download_test_data("multishell_output", data_dir))
+    dataset_dir = Path(dataset_dir) / "multishell_output" / "qsiprep"
+    data_stem = str(dataset_dir / "sub-ABCD" / "dwi" / dwi_prefix)
+
+    # Test actual data (including a nifti)
+    grad_select = GradientSelect(
+        dwi_file=data_stem + ".nii.gz",
+        bval_file=data_stem + ".bval",
+        bvec_file=data_stem + ".bvec",
+        b_file=data_stem + ".b",
+        bval_distance_cutoff=100,
+        expected_n_input_shells=5,
+        requested_shells=[0, "lowest", "highest"],
+    )
+    grad_select.run()
+    correct_n = 73
+    sel_nii = nb.load(dwi_prefix + "_selected.nii.gz")
+    assert sel_nii.shape[3] == correct_n
+    sel_bval = np.loadtxt(dwi_prefix + "_selected.bval")
+    assert sel_bval.shape == (correct_n,)
+    sel_bvec = np.loadtxt(dwi_prefix + "_selected.bvec")
+    assert sel_bvec.shape == (3, correct_n)
+    sel_b = np.loadtxt(dwi_prefix + "_selected.b")
+    assert sel_b.shape[0] == correct_n
+    # There is no btable from this dataset because it was created
+    # before those were written in the outputs.
+    assert not Path(dwi_prefix + "_selected.txt").exists()
+
+
+@pytest.mark.interfaces
+def test_real_shells():
+    # Check some other sequences we might run into
+    # ABCD has 4 shells + b=0s
+    abcd_bval = np.loadtxt(load_data("schemes/ABCD.bval"))
+    abcd_df = _find_shells(abcd_bval, 100)
+    assert abcd_df["shell_num"].max() == 5.0
+
+    # HCP has 3 shells + b=0s
+    hcp_bval = np.loadtxt(load_data("schemes/HCP.bval"))
+    hcp_df = _find_shells(hcp_bval, 100)
+    assert hcp_df["shell_num"].max() == 4.0
+
+    # DSIQ5 should raise an exception
+    dsi_bval = np.loadtxt(load_data("schemes/DSIQ5.bval"))
+    with pytest.raises(Exception, match="Too many possible shells detected."):
+        _find_shells(dsi_bval, 100)
+
+    # Some other assorted test schemes that should fail
+    nonshelled_schemes = ["HASC55-1", "HASC55-2", "HASC92", "RAND57"]
+    for scheme in nonshelled_schemes:
+        bval_file = Path(get_test_data_path()) / f"{scheme}.bval"
+        test_bvals = np.loadtxt(bval_file)
+
+        with pytest.raises(Exception, match="Too many possible shells detected."):
+            _find_shells(test_bvals, 100)
+
+    # DSIQ7 actually _passes_ the mrtrix test, but failes silhouette
+    bval_file = Path(get_test_data_path()) / "Q7.bval"
+    test_bvals = np.loadtxt(bval_file)
+
+    with pytest.raises(Exception, match="Silhouette score is low. Is this is a DSI scheme?"):
+        _find_shells(test_bvals, 100)
