8361ac83c8726b5c589ecf08e3bdd1265bd2ef9f
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-06-27 13:34:12 -0400

    Update reports.py

--- qsirecon/interfaces/reports.py
+++ qsirecon/interfaces/reports.py
@@ -412,6 +412,7 @@ class ScalarReport(SimpleInterface):
         z_cuts = cuts["z"]
         for i_scalar, scalar_map in enumerate(self.inputs.scalar_maps):
             scalar_name = self.inputs.scalar_metadata[i_scalar]["metadata"]["Description"]
+            raise Exception(scalar_map)
             plot_scalar_map(
                 underlay=resampled_underlay,
                 overlay=scalar_map,
@@ -474,7 +475,6 @@ def plot_scalar_map(
         dfs.append(df)
 
     df = pd.concat(dfs, axis=0)
-    raise Exception(df)
     ax0, ax1, ax2 = axes
     with sns.axes_style("whitegrid"), sns.plotting_context(font_scale=3):
         sns.kdeplot(
