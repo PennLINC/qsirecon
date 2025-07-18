99885205606ab4b797aa77d96137f6e9a7f222b9
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-07-18 10:25:09 -0400

    Write out ACPC-space scalars from TORTOISE node if estimate_mapmri is False (#254)
    * Draft test for anat collection.
    
    * Update config.yml
    
    * Keep working.
    
    * Run black.
    
    * Bump niworkflows version.
    
    * Get test working.
    
    * Move function to utils.
    
    * Add test.
    
    * Update build_workflow.py
    
    * Update build_workflow.py
    
    * Keep working.
    
    * Update tortoise.py
    
    * Update tortoise.py

--- qsirecon/cli/parser.py
+++ qsirecon/cli/parser.py
@@ -29,6 +29,7 @@ from argparse import Action
 from pathlib import Path
 
 from .. import config
+from ..utils.bids import get_iterable_dwis_and_anats
 
 
 class ToDict(Action):
@@ -615,50 +616,4 @@ def parse_args(args=None, namespace=None):
         )
 
     config.execution.participant_label = sorted(participant_label)
-    config.execution.processing_list = _get_iterable_dwis_and_anats()
-
-
-def _get_iterable_dwis_and_anats():
-    """Look through the BIDS Layout for DWIs and their corresponding anats.
-
-    Returns
-    -------
-    dwis_and_anats : list of tuple
-        List of two-element tuples where the first element is a DWI scan and the second is
-        the corresponding anatomical scan.
-    """
-    from bids.layout import Query
-
-    dwis_and_anats = []
-    dwi_files = config.execution.layout.get(
-        suffix="dwi",
-        session=Query.OPTIONAL,
-        space=["T1w", "ACPC"],
-        extension=["nii", "nii.gz"],
-    )
-
-    for dwi_scan in dwi_files:
-        subject_level_anats = config.execution.layout.get(
-            suffix=["T1w", "T2w"],
-            session=Query.NONE,
-            space=[Query.NONE, "ACPC"],
-            extension=["nii", "nii.gz"],
-        )
-
-        session_level_anats = []
-        if dwi_session := dwi_scan.entities.get("session"):
-            session_level_anats = config.execution.layout.get(
-                suffix=["T1w", "T2w"],
-                session=dwi_session,
-                space=[Query.NONE, "ACPC"],
-                extension=["nii", "nii.gz"],
-            )
-
-        if not (session_level_anats or subject_level_anats):
-            anat_scan = None
-        else:
-            best_anat_source = session_level_anats if session_level_anats else subject_level_anats
-            anat_scan = best_anat_source[0]
-
-        dwis_and_anats.append((dwi_scan, anat_scan))
-    return dwis_and_anats
+    config.execution.processing_list = get_iterable_dwis_and_anats(layout=config.execution.layout)
