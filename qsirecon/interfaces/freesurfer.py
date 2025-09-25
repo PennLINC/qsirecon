05e86f53af76d9b25b89468c3bacadea42ab8502
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-09-25 16:30:39 -0400

    Reapply "Fix find_fs_path."
    This reverts commit ffce04108cf0559e981eb7589013043e0e9cde6a.

--- qsirecon/interfaces/freesurfer.py
+++ qsirecon/interfaces/freesurfer.py
@@ -22,18 +22,45 @@ Disable warnings:
 import os.path as op
 from pathlib import Path
 
+from bids.utils import listify
+
 
 def find_fs_path(freesurfer_dir, subject_id, session_id=None):
-    """Find a freesurfer dir for subject or subject+session."""
+    """Find a freesurfer dir for subject or subject+session.
+
+    Parameters
+    ----------
+    freesurfer_dir : str
+        The directory containing the freesurfer outputs.
+    subject_id : str
+        The subject ID.
+    session_id : list or str or None, optional
+        The session ID.
+        May be None, a string, or a list containing strings and Query.NONE objects.
+
+    Returns
+    -------
+    path : Path or None
+        The path to the freesurfer directory.
+    """
 
     if freesurfer_dir is None:
         return None
 
     # Look for longitudinal pipeline outputs first
-    if session_id is not None:
+    # Since session_id can be a two-item list with a session and a Query.NONE,
+    # or just a string with the session, or None, we need to extract the actual session ID
+    # robustly, when it's present.
+    session_id = listify(session_id)
+    session_id = [ses_id for ses_id in session_id if isinstance(ses_id, str)]
+    if session_id:
+        # There should only be one actual session ID
+        session_id = session_id[0]
+
         nosub = op.join(freesurfer_dir, f"{subject_id}_{session_id}.long.{subject_id}")
         if op.exists(nosub):
             return Path(nosub)
+
         withsub = op.join(
             freesurfer_dir,
             f"sub-{subject_id}_ses-{session_id}.long.sub-{subject_id}",
@@ -45,14 +72,18 @@ def find_fs_path(freesurfer_dir, subject_id, session_id=None):
         nosub = op.join(freesurfer_dir, f"{subject_id}_{session_id}")
         if op.exists(nosub):
             return Path(nosub)
+
         withsub = op.join(freesurfer_dir, f"sub-{subject_id}_ses-{session_id}")
         if op.exists(withsub):
             return Path(withsub)
 
+    # Then look for cross-sectional pipeline outputs
     nosub = op.join(freesurfer_dir, subject_id)
     if op.exists(nosub):
         return Path(nosub)
+
     withsub = op.join(freesurfer_dir, f"sub-{subject_id}")
     if op.exists(withsub):
         return Path(withsub)
+
     return None
