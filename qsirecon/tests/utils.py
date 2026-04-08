95aa26ee6025d906ae1d1230e09e2f247bee6baf
Taylor Salo <tsalo90@gmail.com>
2026-04-08 10:41:48 -0400

    Try to fix CI

--- qsirecon/tests/utils.py
+++ qsirecon/tests/utils.py
@@ -1,78 +1,25 @@
 """Utility functions for tests."""
 
 import base64
-import lzma
 import os
-import tarfile
 from glob import glob
-from gzip import GzipFile
-from io import BytesIO
 
-import requests
 from nipype import logging
 
 from qsirecon import config
+
+from qsirecon.tests.integration_test_data import download_test_data as _download_test_data
 
 LOGGER = logging.getLogger('nipype.utils')
 
 
 def download_test_data(dset, data_dir=None):
     """Download test data."""
-    URLS = {
-        'multishell_output': (
-            'https://upenn.box.com/shared/static/q066t33ojqk1phljr6064ilcrrqip87q.gz'
-        ),
-        'singleshell_output': (
-            'https://upenn.box.com/shared/static/rc4oh9tqf20f2xu2p8587q44vwr1i7uf.gz'
-        ),
-        'hsvs_data': ('https://upenn.box.com/shared/static/kic35jscwqhtezi6t398k4nfuo09ppty.xz'),
-        'multises_pre1_output': (
-            'https://upenn.box.com/shared/static/g6hb4ylraejqn8sjz2xodj6fog4lp6sb.xz'
-        ),
-        'multises_post1_output': (
-            'https://upenn.box.com/shared/static/ipqhy6a9p0pl7q1tw4zejj47mro4dtfh.xz'
-        ),
-        'custom_atlases': (
-            'https://upenn.box.com/shared/static/2x5jqj7he20lminc3v4jtwhyiq7pyr8i.gz'
-        ),
-    }
-    if dset == '*':
-        for k in URLS:
-            download_test_data(k, data_dir=data_dir)
-
-        return
-
-    if dset not in URLS:
-        raise ValueError(f'dset ({dset}) must be one of: {", ".join(URLS.keys())}')
-
-    if not data_dir:
-        data_dir = os.path.join(os.path.dirname(get_test_data_path()), 'test_data')
-
-    out_dir = os.path.join(data_dir, dset)
-
-    if os.path.isdir(out_dir):
-        config.loggers.utils.info(
-            f'Dataset {dset} already exists. '
-            'If you need to re-download the data, please delete the folder.'
-        )
-        return out_dir
-    else:
-        config.loggers.utils.info(f'Downloading {dset} to {out_dir}')
-
-    os.makedirs(out_dir, exist_ok=True)
-    url = URLS[dset]
-    with requests.get(url, stream=True) as req:
-        if url.endswith('.xz'):
-            with lzma.open(BytesIO(req.content)) as f:
-                with tarfile.open(fileobj=f) as t:
-                    t.extractall(out_dir)
-        elif url.endswith('.gz'):
-            with tarfile.open(fileobj=GzipFile(fileobj=BytesIO(req.content))) as t:
-                t.extractall(out_dir)
-        else:
-            raise ValueError(f'Unknown file type for {dset} ({url})')
-
-    return out_dir
+    return _download_test_data(
+        dset,
+        data_dir=data_dir,
+        info=config.loggers.utils.info,
+    )
 
 
 def get_test_data_path():
