3d95f64eb5ec540774d223d6144380d821a8b076
Taylor Salo <tsalo90@gmail.com>
2026-04-09 10:04:16 -0400

    Run ruff.

--- qsirecon/cli/parser.py
+++ qsirecon/cli/parser.py
@@ -476,7 +476,9 @@ def parse_args(args=None, namespace=None):
     opts.datasets = opts.datasets or {}
     if opts.atlases:
         if 'qsireconatlases' not in opts.datasets:
-            opts.datasets['qsireconatlases'] = Path('/home/qsirecon/.cache/qsirecon/QSIReconAtlases')
+            opts.datasets['qsireconatlases'] = Path(
+                '/home/qsirecon/.cache/qsirecon/QSIReconAtlases'
+            )
             if not opts.datasets['qsireconatlases'].is_dir():
                 raise NotADirectoryError(
                     f'QSIRecon atlases is not a directory: {opts.datasets["qsireconatlases"]}'
